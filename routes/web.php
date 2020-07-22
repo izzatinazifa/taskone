4c4866d5e24c344c7971d37969045c7868f4801d
nazifa <nazifa@mynic.my>
2020-07-22 11:08:07 +0800

    git init

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
+    error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+}
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/tasks','TaskController');
